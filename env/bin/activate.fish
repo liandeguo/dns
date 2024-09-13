c3cbfdec816f0d39973999f503805cab46c7d256
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-09-13 20:36:38 +0200

    got it working

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/liandeguo/Documents/dns/env"
+set -gx VIRTUAL_ENV "/Users/liandeguo/Documents/octodns/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
